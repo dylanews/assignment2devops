04ee74bba3c723f7f68367386747667771ff041c
Dylan <s3699661@student.rmit.edu.au>
2020-05-05 15:22:35 +0800

    first commit

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,19 +1,23 @@
-output "instance_public_ip" {
-  value = 
-}
+# output "instance_public_ip" {
+#   value = 
+# }
 
-output "lb_endpoint" {
-  value = 
-}
+# output "lb_endpoint" {
+#   value = 
+# }
 
-output "db_endpoint" {
-  value = 
-}
+# output "db_endpoint" {
+#   value = 
+# }
 
-output "db_user" {
-  value = 
-}
+# output "db_user" {
+#   value = 
+# }
 
-output "db_pass" {
-  value = 
+# output "db_pass" {
+#   value = 
+# }
+
+output "endpoint" {
+  value = aws_lb.assignment2.dns_name
 }
